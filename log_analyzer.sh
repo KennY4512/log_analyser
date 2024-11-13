690ebb9393aa1b01bbf7591ecb75bc02b4c0b266
LANG Kenny <kenny.lang.pro@gmail.com>
2024-11-13 19:58:03 +0100

    V0.2.0 Modification et finalisation de la fonction inputcheck

--- log_analyzer.sh
+++ log_analyzer.sh
@@ -3,24 +3,35 @@
 # Fonction de vérification des arguments 1 et 2
 inputcheck () {
 
-    # Vérification de la présence et de la bonne syntax de l'argument 1
+    if [ "$#" == "0" ]; then
+        echo -e "\e[31;1mAucun paramètre donné !\n\e[0m"
+        echo -e "\e[31;1mVeuillez spécifier l'un des 2 paramètres ci-dessous suivit d'un fichier log valable :\e[0m"
+        echo -e "\e[31;1maggregate :\e[0m \e[31magrégation du fichier log\e[0m"
+        echo -e "\e[31;1mtemporal_analysis :\e[0m \e[31manalyse temporelle du fichier log\n\e[0m"
+        echo -e "\e[31;1mExemple d'appel :\e[0m \e[31m./log_analyser.sh aggregate logfile.log\e[0m"
+        exit 1
+    fi
+
+    # Vérification de la présence et de la syntax de l'argument 1
     if [ "$1" == "" ]; then
-        echo -e "\e[31;1mErreur :\nVeuillez fournir une action à réaliser (aggregate - temporal_analysis)\e[0m"
+        echo -e "\e[31;1mErreur :\nVeuillez fournir une action à réaliser ! (aggregate - temporal_analysis)\e[0m"
         exit 1
     elif [ "$1" != "aggregate" -a "$1" != "temporal_analysis" ]; then
-        echo -e "\e[31;1mErreur :\nSyntax incorrecte dans le premier argument (aggregate - temporal_analysis)\e[0m"
+        echo -e "\e[31;1mErreur :\nSyntax incorrecte dans le premier paramètre ! (aggregate - temporal_analysis)\e[0m"
         exit 1
     fi
 
-    # Vérification de la présence et de la bonne syntax de l'argument 2
+    # Vérification de la présence, de la bonne syntax de l'argument 2 et du fichier log fourni
     if [ "$2" == "" ]; then
-        echo -e "\e[31;1mErreur :\nVeuillez fournir un fichier log (.log) à analyser\e[0m"
+        echo -e "\e[31;1mErreur :\nVeuillez fournir un fichier de log à analyser !\e[0m"
         exit 1
-    elif [[ ! "$2" =~ \.log$ ]]; then
-        echo -e "\e[31;1mErreur :\nLe fichier fourni n'est pas un fichier .log\e[0m"
+    elif [[ ! -f "$2" ]]; then
+        echo -e "\e[31;1mErreur :\nLe fichier de log fourni est introuvable !\e[0m"
+        exit 1
+    elif ! grep -q '^\[[A-Za-z]*\] [0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}T[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}.[0-9]\++[0-9]\{2\}:[0-9]\{2\} msg=".*"' $2; then
+        echo -e "\e[31;1mErreur :\nLe contenu du fichier de log fourni n'est pas au bon format !\e[0m"
         exit 1
     fi
-
 }
 
 # Fonction de parsage et d'agrégation des donnés des logs
